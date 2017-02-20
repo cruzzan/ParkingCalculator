077e6551d0a36c9cfe53a75ceeb472181552ea15
Sebastian Hultstrand <sebastian@storm-net.se>
2017-02-20 22:00:08 +0100

    Fixed up the testrunning through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var elixir = require('laravel-elixir');
 var gulp = require('gulp');
+var phpunit = require('gulp-phpunit');
 
 /*
  |--------------------------------------------------------------------------
@@ -17,7 +18,5 @@ elixir(function(mix) {
 });
 
 gulp.task('phpunit', function() {
-    elixir(function(mix) {
-        mix.phpUnit();
-    });
+    gulp.src('phpunit.xml').pipe(phpunit());
 });
